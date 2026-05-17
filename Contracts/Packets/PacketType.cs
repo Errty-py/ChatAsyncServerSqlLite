58e797da0f09799d950839e01aa557ab3243f0b1
@_errty.r <l1ntegithub@gmail.com>
2026-05-17 15:11:16 +0300

    normalized PacketType for using in json Requests

--- Contracts/Packets/PacketType.cs
+++ Contracts/Packets/PacketType.cs
@@ -1,5 +1,8 @@
-﻿namespace ChatAsyncServerSqlLite.Contracts.Packets;
+﻿using System.Text.Json.Serialization;
 
+namespace ChatAsyncServerSqlLite.Contracts.Packets;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum PacketType
 {
     Registration,
